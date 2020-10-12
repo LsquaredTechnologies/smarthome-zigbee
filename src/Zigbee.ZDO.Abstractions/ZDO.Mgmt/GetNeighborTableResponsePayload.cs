78cd6a45c248794e4c2110bcd2d1f1e5bc836e3d
LionelL <lionel.lalande+github@lsquared.fr>
2020-10-12 20:19:06 +0200

    Add SrcNwkAddr

--- src/Zigbee.ZDO.Abstractions/ZDO.Mgmt/GetNeighborTableResponsePayload.cs
+++ src/Zigbee.ZDO.Abstractions/ZDO.Mgmt/GetNeighborTableResponsePayload.cs
@@ -1,9 +1,9 @@
 namespace Lsquared.SmartHome.Zigbee.ZDO.Mgmt
 {
     // 0x8031
-    public sealed record GetNeighborTableResponsePayload(byte Status, byte Capacity, byte StartIndex, Array<NeighborTableEntry> NeighborTableEntries) : ICommandPayload
+    public sealed record GetNeighborTableResponsePayload(byte Status, NWK.Address SrcNwkAddr, byte Capacity, byte StartIndex, Array<NeighborTableEntry> NeighborTableEntries) : ICommandPayload
     {
         public override string ToString() =>
-           $"<{GetType()}> Status: {Status:X2}, Capacity: {Capacity}, StartIndex: {StartIndex}"; // TODO show entries
+           $"<{GetType()}> Status: {Status:X2}, SrcNwkAddr: {SrcNwkAddr:X4}, Capacity: {Capacity}, StartIndex: {StartIndex}"; // TODO show entries
     }
 }
