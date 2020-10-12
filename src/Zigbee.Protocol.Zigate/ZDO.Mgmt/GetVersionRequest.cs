24a33cf83356a772e7f62efc1ef546512e170d6f
LionelL <lionel.lalande+github@lsquared.fr>
2020-10-12 20:20:00 +0200

    Add expected response code

--- src/Zigbee.Protocol.Zigate/ZDO.Mgmt/GetVersionRequest.cs
+++ src/Zigbee.Protocol.Zigate/ZDO.Mgmt/GetVersionRequest.cs
@@ -4,6 +4,8 @@ namespace Lsquared.SmartHome.Zigbee.ZDO.Mgmt
 {
     public sealed record GetVersionRequest : Request
     {
+        public override ushort ExpectedResponseCode => 0x8010;
+
         public GetVersionRequest() : base(new ZigateCommandHeader(0x0010, 0), ICommandPayload.None) { }
     }
 }
